cc6d61816ba9945474792963c67ac4d1f0362d00
Hydr00 <43877939+Hydr00@users.noreply.github.com>
2019-10-19 23:59:09 +0800

    更改扫码页面

--- Cateyes/Scanfunction.js
+++ Cateyes/Scanfunction.js
@@ -1,19 +1,3 @@
-document.getElementById("headder_mask").addEventListener("click", function() {
-	if (navigator.userAgent.indexOf("Html5Plus") > -1) {
-		var W = plus.webview.create('notfind.html', '404', {
-			'titleNView': {
-				'backgroundColor': '#262930',
-				'titleText': '页面不见了！',
-				'titleColor': '#FFFFFF',
-				autoBackButton: true
-			}
-		});
-		plus.webview.show(W, 'slide-in-right', '300ms');
-	} else {
-		alert('不在手机环境中');
-	}
-});
-
 document.getElementById("headder_scaniconcontaier").addEventListener("click", function() {
 	if (navigator.userAgent.indexOf("Html5Plus") > -1) {
 		var W = plus.webview.create('Scanwindow.html', 'scan', {
