eb8a14b900d583b753838e252ea5a0bf38358a62
Hydr00 <43877939+Hydr00@users.noreply.github.com>
2019-10-19 22:20:19 +0800

    增加404界面

--- Cateyes/Scanfunction.js
+++ Cateyes/Scanfunction.js
@@ -13,3 +13,19 @@ document.getElementById("headder_scaniconcontaier").addEventListener("click", fu
 		alert('请在APP中扫码');
 	}
 });
+
+document.getElementById("headder_mask").addEventListener("click", function() {
+	if (navigator.userAgent.indexOf("Html5Plus") > -1) {
+		var W = plus.webview.create('404', 'carcode1', {
+			'titleNView': {
+				'backgroundColor': '#262930',
+				'titleText': '页面不见了！',
+				'titleColor': '#FFFFFF',
+				autoBackButton: true
+			}
+		});
+		plus.webview.show(W, 'slide-in-right', '300ms');
+	} else {
+		alert('不在手机环境中');
+	}
+});
