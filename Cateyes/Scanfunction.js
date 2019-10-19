b67bb2bbb9d2fab866bd88de7db3ccea6ce610a8
Hydr00 <43877939+Hydr00@users.noreply.github.com>
2019-10-19 22:31:52 +0800

    Update Scanfunction.js

--- Cateyes/Scanfunction.js
+++ Cateyes/Scanfunction.js
@@ -1,19 +1,3 @@
-document.getElementById("headder_scaniconcontaier").addEventListener("click", function() {
-	if (navigator.userAgent.indexOf("Html5Plus") > -1) {
-		var W = plus.webview.create('Scanwindow.html', 'scan', {
-			'titleNView': {
-				'backgroundColor': '#262930',
-				'titleText': '扫一扫',
-				'titleColor': '#FFFFFF',
-				autoBackButton: true
-			}
-		});
-		plus.webview.show(W, 'slide-in-right', '300ms');
-	} else {
-		alert('请在APP中扫码');
-	}
-});
-
 document.getElementById("headder_mask").addEventListener("click", function() {
 	if (navigator.userAgent.indexOf("Html5Plus") > -1) {
 		var W = plus.webview.create('404.html', '404', {
@@ -27,5 +11,21 @@ document.getElementById("headder_mask").addEventListener("click", function() {
 		plus.webview.show(W, 'slide-in-right', '300ms');
 	} else {
 		alert('不在手机环境中');
+	}
+});
+
+document.getElementById("headder_scaniconcontaier").addEventListener("click", function() {
+	if (navigator.userAgent.indexOf("Html5Plus") > -1) {
+		var W = plus.webview.create('Scanwindow.html', 'scan', {
+			'titleNView': {
+				'backgroundColor': '#262930',
+				'titleText': '扫一扫',
+				'titleColor': '#FFFFFF',
+				autoBackButton: true
+			}
+		});
+		plus.webview.show(W, 'slide-in-right', '300ms');
+	} else {
+		alert('请在APP中扫码');
 	}
 });
